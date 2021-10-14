9dc8d0af1a7ea153ac92ab626836f83a7c653684
BalajiNagargoje <balajinagargoje2410@gmail.com>
2021-10-14 23:14:44 +0530

    IES:1 Project initialy created

---
+++ src/main/java/com/samol/SachinGitPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.samol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SachinGitPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SachinGitPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samol/SachinGitPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SachinGitPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
